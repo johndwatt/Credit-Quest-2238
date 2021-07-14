97baeef0cbe028741cf97dd9fbc9c13fddcfd84a
johndwatt <jdw23@protonmail.com>
2021-07-13 20:33:41 -0700

    add method of increasing credits every second once start button has been pushed

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
-//console.log("Hello World");
 const game = {
     credits: 0,
     fuel: 10,
@@ -6,12 +5,29 @@ const game = {
     radiation: 0,
     startGame (){
         game.grabShipName();
+        game.countCredits();
     }, 
     grabShipName (){
         const shipName = $("#spaceship-name").val();
         $("#named-ship").text(shipName);
-    }
+    },
+    countCredits (){
+        //console.log("CC works");
+        //when button is clicked - DONE, credits go up by a certain amount depending on how many credits the user already has. 
+        setInterval(function (){
+            if (game.credits < 10000) {
+                game.credits = game.credits += 100;
+                //console.log(game.credits);
+            } else if (game.credits >= 100000) {
+                game.credits = game.credits += 5000;
+                //console.log(game.credits);
+            } else {
+                game.credits = game.credits += 1000;
+                //console.log(game.credits);
+            }
+        }, 1000);
+    },
     
 }
-//console.log(game.startGame());
+
 $("#start-btn").on("click", game.startGame);
